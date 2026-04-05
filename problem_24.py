5a67b479693473afb26d53837c4ea09acc9f79fc
Fabio Antonio Astore <astore.a.fabio@gmail.com>
2026-04-05 17:04:03 -0300

    problem 24

---
+++ problem_24.py
@@ -0,0 +1,24 @@
+CHARACTERS = [chr(i) for i in range(ord("0"), ord("9") + 1)]
+PERMUTATION_POSITION = 10**6
+
+
+def gen_permutations(item_list: list[str], permutation: str = ""):
+    for item in item_list:
+        temp_permutation = permutation + item
+        temp_set = item_list.copy()
+        temp_set.remove(item)
+        if len(temp_set) == 0:
+            yield temp_permutation
+        else:
+            yield from gen_permutations(temp_set, temp_permutation)
+
+
+millionth_permutation = None
+index = 1
+for p in gen_permutations(CHARACTERS):
+    if index == PERMUTATION_POSITION:
+        millionth_permutation = p
+        break
+    index += 1
+
+print(millionth_permutation)
